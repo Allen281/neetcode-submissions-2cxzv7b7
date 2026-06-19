48097f62c3dbcce3eb441e3919d187b5b952c95a
Allen <72230871+Allen281@users.noreply.github.com>
2026-06-19 18:46:48 -0500

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        UPPER_BOUND = 2**31 - 1
+        LOWER_BOUND = 2**31
+
+        rslt = 0
+        isNeg = x < 0
+        x = abs(x)
+
+        while x > 0:
+            digit = x%10
+
+            if not isNeg and (UPPER_BOUND-digit) // 10 < rslt:
+                return 0
+
+            if isNeg and (LOWER_BOUND-digit) // 10 < rslt:
+                return 0
+            
+            rslt = rslt*10 + digit
+            x //= 10
+        
+        return -rslt if isNeg else rslt
