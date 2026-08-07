3be8d628b53b8c89ccdd7234d734a4e294ed1991
Allen <72230871+Allen281@users.noreply.github.com>
2026-08-06 22:56:24 -0700

    Add: decode-string - submission-5

---
+++ Data Structures & Algorithms/decode-string/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        parsed = [x for x in re.split(r'(\d+)|([\[\]])', s) if x]
+        nums = []
+        letters = []
+        for p in parsed:
+            if p.isdigit():
+                nums.append(int(p))
+            elif p == ']':
+                mult = nums.pop()
+                rslt = mult*letters.pop()
+
+                if letters:
+                    letters.pop()
+                if letters and letters[-1].isalpha():
+                    letters[-1] += rslt
+                else:
+                    letters.append(rslt)
+            else:
+                if p.isalpha() and letters and letters[-1] != '[':
+                    letters[-1] += p
+                else:
+                    letters.append(p)
+        
+        return "".join(letters)
